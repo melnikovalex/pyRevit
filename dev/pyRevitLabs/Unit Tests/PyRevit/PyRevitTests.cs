fcddfb10b4f1c9681cfae1271f16c7a458d5c3b0
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-02-19 17:42:10 -0800

    Started untitest project for pyrevitlabs

---
+++ dev/pyRevitLabs/Unit Tests/PyRevit/PyRevitTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using pyRevitLabs.TargetApps.Revit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pyRevitLabs.TargetApps.Revit.Tests {
+    [TestClass()]
+    public class PyRevitTests {
+        [TestMethod()]
+        public void GetAttachedTest() {
+            Assert.Fail();
+        }
+    }
+}
