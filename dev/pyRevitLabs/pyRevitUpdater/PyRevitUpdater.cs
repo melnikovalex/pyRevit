76a84fe5cda8b48ede04d04b0b97a1387f2b119c
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-02-22 09:44:59 -0800

    updater mechanism cleanups step 2

--- dev/pyRevitLabs/pyRevitUpdater/PyRevitUpdater.cs
+++ dev/pyRevitLabs/pyRevitUpdater/PyRevitUpdater.cs
@@ -1,32 +1,45 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Windows;
 
-using pyRevitLabs.CommonCLI;
 using pyRevitLabs.TargetApps.Revit;
 
 namespace pyRevitUpdater {
     public class PyRevitUpdaterCLI {
         static void Main(string[] args) {
             if (args.Length >= 1) {
+                // grab clone from args
                 var clonePath = args[0];
+
+                // request update
                 RunUpdate(clonePath);
             }
         }
 
         public static void RunUpdate(string clonePath) {
             try {
+                // find target clone
                 var clone = PyRevit.GetRegisteredClone(clonePath);
+
+                // run update
                 PyRevit.Update(clone);
+
+                // write success message to system logs
                 using (EventLog eventLog = new EventLog("Application")) {
                     eventLog.Source = "Application";
-                    eventLog.WriteEntry("Log message example", EventLogEntryType.Information, 101, 1);
+                    eventLog.WriteEntry(
+                        string.Format("Successfully Updated Clone \"{0}\"", clone.Name),
+                        EventLogEntryType.Information
+                        );
                 }
             }
             catch (Exception ex){
+                // write error message to system logs
                 using (EventLog eventLog = new EventLog("Application")) {
-                    eventLog.Source = "Application";
-                    eventLog.WriteEntry("Log message example", EventLogEntryType.Information, 101, 1);
+                    eventLog.Source = "pyRevit Updater";
+                    eventLog.WriteEntry(
+                        string.Format("Update Error: {0}", ex.Message),
+                        EventLogEntryType.Error
+                        );
                 }
             }
         }
