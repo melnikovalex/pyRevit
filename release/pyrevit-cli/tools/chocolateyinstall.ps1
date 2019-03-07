ceda867d85d59936337b423f4cf15f8658ff2f1a
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-03-07 10:00:23 -0800

    Edited choco scripts per moderator comments

--- release/pyrevit-cli/tools/chocolateyinstall.ps1
+++ release/pyrevit-cli/tools/chocolateyinstall.ps1
@@ -7,15 +7,12 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = $url
-  url64bit      = 'https://github.com/eirannejad/pyRevit/releases/download/cli-v0.9.0.0/pyRevit.CLI_0.9.0.0_signed.exe'
+  url           = 'https://github.com/eirannejad/pyRevit/releases/download/cli-v0.9.0.0/pyRevit.CLI_0.9.0.0_signed.exe'
 
   softwareName  = 'pyrevit-cli*'
 
   checksum      = '0655648B5698FC2B9B22B650CC411AF07302A295E9C0C617B143A13447D689E3'
   checksumType  = 'sha256'
-  checksum64    = '0655648B5698FC2B9B22B650CC411AF07302A295E9C0C617B143A13447D689E3'
-  checksumType64= 'sha256'
 
   silentArgs    = "/exenoui /exenoupdates /qn"
   validExitCodes= @(0, 3010, 1641)
