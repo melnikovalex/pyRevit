76e8a213d290c7ed90ac6d492cafe555106b8092
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-03-17 12:54:38 -0700

    released 0.9.7 on choco
    This process is still under testing and thus here in develop branch

--- release/pyrevit-cli/tools/chocolateyinstall.ps1
+++ release/pyrevit-cli/tools/chocolateyinstall.ps1
@@ -7,11 +7,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'exe'
-  url           = 'https://github.com/eirannejad/pyRevit/releases/download/cli-v0.9.6.0/pyRevit.CLI_0.9.6.0_signed.exe'
+  url           = 'https://github.com/eirannejad/pyRevit/releases/download/cli-v0.9.7.0/pyRevit.CLI_0.9.7.0_signed.exe'
 
   softwareName  = 'pyrevit-cli*'
 
-  checksum      = 'DEDF4BE7DAE8EC7E72C100EBA244B2F992BD2DF33F8360B1CB263221A7133E6C'
+  checksum      = '9F176E1D0394A2706CB63973B96E437974C025D31313192B4C79B9D72D210470'
   checksumType  = 'sha256'
 
   silentArgs    = "/exenoui /exenoupdates /qn"
--- release/pyrevit-cli/tools/chocolateyuninstall.ps1
+++ release/pyrevit-cli/tools/chocolateyuninstall.ps1
@@ -1,4 +1,4 @@
-﻿$productId = '{A72CCEB0-FD16-472D-8FF1-5215981985F3}'
+﻿$productId = '{D5505166-C49A-436F-B25A-9FBD0632F6F0}'
 $silentArgs = '/qn /norestart'
 $validExitCodes = @(0)
 $msiArgs = "/X$productId $silentArgs"
