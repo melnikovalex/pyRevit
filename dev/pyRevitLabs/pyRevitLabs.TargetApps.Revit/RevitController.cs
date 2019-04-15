c405577c3976e94660fdfa17ea5dc68dbdff25c5
Ehsan Iran-Nejad <eirannejad@gmail.com>
2019-04-15 10:10:08 -0700

    Added Revit 2020 build code

--- dev/pyRevitLabs/pyRevitLabs.TargetApps.Revit/RevitController.cs
+++ dev/pyRevitLabs/pyRevitLabs.TargetApps.Revit/RevitController.cs
@@ -422,6 +422,9 @@ namespace pyRevitLabs.TargetApps.Revit {
             {"20181217_1515", ("19.2.0.65",     "2019.2 (Update)")},
             {"20190108_1515", ("19.2.1.1",      "2019.2 (Full Install)")}, // reported by: https://twitter.com/JarodJSchultz/status/1100459171491676160
             {"20190225_1515", ("19.2.10.7",     "2019.2.1")}, // release notes https://up.autodesk.com/2019/RVT/Autodesk_Revit_2019_2_1_Readme.html
+
+            // 2020
+            {"20190327_2315", ("20.0.0.377",    "2020 First Customer Ship")},
         };
 
         private static Regex BuildNumberFinder = new Regex(@".*(?<build>\d{8}_\d{4}).*");
